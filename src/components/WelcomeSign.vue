dde52357065dfdaf06e22c67494de31d5b0d4c5a
Space <space@reversed.dev>
2024-08-26 18:25:22 +0200

    Init

---
+++ src/components/WelcomeSign.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    An app that doesn't require an email to generate a damm qr code. Made with <span style="color: red;">♥️</span> and Vue.JS defaults 💀 <br> <br>The QR code generates live, download whenever you want!
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <div>
+      <h2 class="green">Your URL</h2>
+      <input type="url" name="url" id="url" alt="Your URL to make to a QR Code" oninput='var qr = qrcode(4, "L");qr.addData(document.getElementById("url").value);qr.make();document.getElementById("placeHolder").src = qr.createDataURL();'>
+    </div>
+    <div style="margin-top: 10px;">
+      <div>
+        <img src="" alt="Your QR Code" width="30%" height="30%" id="placeHolder">
+      </div>
+    </div>
+  </main>
+</template>
